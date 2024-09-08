89fbb716075eeffc9a8d18c732de5f256aaa55d1
VishuKalier2003 <vishukalier123@gmail.com>
2024-09-08 18:31:25 +0530

    First update

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import StudentAddForm from "./components/studentDetailsForm"; 
+import './globals.css';
+
+export default function Home() {
+  return (
+    <section>
+      <StudentAddForm />
+    </section>
+  );
+}
